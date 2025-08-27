5e3fcc444a98b987ccce4d18f58c881ea435f749
Izmawarakhan <izmawarakhan123@gmail.com>
2025-08-27 11:18:02 +0500

    Add project files.

---
+++ gitDemoApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Add
+{
+    static void Main(string[] args)
+    {
+        int a = 8;
+        int b = 9;
+        int c = 3333;
+        int d = 199;
+        Console.WriteLine(a + b + c + d);
+    }
+}
+
